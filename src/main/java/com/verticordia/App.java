ffba4ba53dacd78340d05863c8d1771163f75d95
Lucca Pellegrini <632054@sga.pucminas.br>
2025-02-14 16:00:53 -0300

    App.java: Aplica configurações de formatação
    Aplica as configurações definidas no `.editorconfig` em 4f2d8b2 usando a
    formatação automática do Neovim por meio do formatador `clang_format`.

--- src/main/java/com/verticordia/App.java
+++ src/main/java/com/verticordia/App.java
@@ -5,7 +5,7 @@ package com.verticordia;
  *
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
