b08ea03bda83b3fe85430707aed4893a10a88053
Lucca Pellegrini <632054@sga.pucminas.br>
2025-02-19 16:18:01 -0300

    Menu: aplica formatação a `Menu.java`

--- src/main/java/com/verticordia/AEDs3/Menu.java
+++ src/main/java/com/verticordia/AEDs3/Menu.java
@@ -1,39 +1,32 @@
 package com.verticordia.AEDs3;
+
 import java.util.Scanner;
 
 public class Menu {
-    
-    public static void main(String[] args) {
-
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int acao = 10;
 
-		System.out.println("Bem vindo ao menu do CRUD!!! Digite o seguintes números para a ação que desejar: ");
+		System.out.println("Bem vindo ao menu do CRUD!!! Digite o seguintes números para a ação que desejar:");
 
-		while(acao != 0){
-
+		while (acao != 0) {
 			System.out.println("1. Create\n2. Read\n3. Update\n4. Delete\n0. Parar o programa.");
 			acao = sc.nextInt();
 
-			if(acao == 1){
-				//codigo do create
-			}
-			else if(acao == 2){
-				//codigo do read
-			}
-			else if(acao == 3){
-				//codigo do update
-			}
-			else if(acao == 4){
-				//codigo do delete
-			}
-			else if(acao == 0) {
+			if (acao == 1) {
+				// codigo do create
+			} else if (acao == 2) {
+				// codigo do read
+			} else if (acao == 3) {
+				// codigo do update
+			} else if (acao == 4) {
+				// codigo do delete
+			} else if (acao == 0) {
 				System.out.println("Programa finalizado!");
-			}
-			else{
+			} else {
 				System.out.println("Tente outro número.");
 			}
-	}
+		}
 
 		sc.close();
 	}
