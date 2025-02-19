2937e02738116188236845d7df77729c5d777f26
Lucca Pellegrini <632054@sga.pucminas.br>
2025-02-19 14:21:46 -0300

    .editorconfig: corrige opção `insert_final_newline`
    Essa opção, corretamente configurada, permite que limpemos a output de
    `git diff` e semelhantes, pois a última linha de cada arquivo não
    contará mais como modificada se uma nova linha for adicionada.

--- src/main/java/com/verticordia/AEDs3/App.java
+++ src/main/java/com/verticordia/AEDs3/App.java
@@ -3,11 +3,11 @@ package com.verticordia.AEDs3;
 import java.io.IOException;
 
 public class App {
-    public static void main(String[] args) throws IOException {
-        CSVManager csv = new CSVManager("dataset.csv");
-        TrackDB db = new TrackDB("tracks.db");
+	public static void main(String[] args) throws IOException {
+		CSVManager csv = new CSVManager("dataset.csv");
+		TrackDB db = new TrackDB("tracks.db");
 
 		for (Track track : csv)
 			db.add(track);
-    }
+	}
 }
