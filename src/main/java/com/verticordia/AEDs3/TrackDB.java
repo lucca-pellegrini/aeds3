bdeebf85f627efa9a2d4531b0eb56692e2b60958
Lucca Pellegrini <632054@sga.pucminas.br>
2025-02-22 17:45:08 -0300

    TrackDB: adiciona classe concreta BinaryTrackReader
    Assim como temos BinaryTrackWriter para salvar nossas Tracks no TrackDB,
    criamos também uma classe BinaryTrackReader para ler as Tracks do
    TrackDB. Seu construtor recebe os parâmetros `valid` e `size` do
    TrackDB, logo, esses campos têm que ser lidos antes de construir o
    objeto. Também recebe uma “input stream”, que será salva localmente, e
    só será processada se invocarmos o método `getTrack()`. Isso evita que
    tenhamos que processar a stream até que exigido, o que permitirá
    implementar Updates diretos no futuro, onde não é necessário criar o
    objeto Track na memória (ou mesmo carregar a input inteira à memória),
    para atualizar campos de tamanho fixo.

--- src/main/java/com/verticordia/AEDs3/TrackDB.java
+++ src/main/java/com/verticordia/AEDs3/TrackDB.java
@@ -70,6 +70,69 @@ abstract class BinaryTrack {
 	}
 }
 
+class BinaryTrackReader extends BinaryTrack {
+	protected ByteArrayInputStream stream;
+	private Track track;
+
+	public BinaryTrackReader(boolean valid, int size, ByteArrayInputStream stream) {
+		track = null;
+		this.valid = valid;
+		this.size = size;
+		this.stream = stream;
+	}
+
+	private void readTrack() throws IOException {
+		DataInputStream dataStream = new DataInputStream(stream);
+
+		int id = dataStream.readInt();
+		String name = dataStream.readUTF();
+		int numArtists = dataStream.readByte();
+		List<String> trackArtists = new ArrayList<String>(numArtists);
+		for (int i : new Range(numArtists))
+			trackArtists.add(dataStream.readUTF());
+		String albumName = dataStream.readUTF();
+		LocalDate albumReleaseDate = LocalDate.ofEpochDay(dataStream.readLong() / 86400);
+		String albumType = dataStream.readUTF();
+		int numGenres = dataStream.readByte();
+		List<String> genres = new ArrayList<String>(numGenres);
+		for (int i : new Range(numGenres))
+			genres.add(dataStream.readUTF());
+		boolean explicit = dataStream.readBoolean();
+		byte[] trackIdBytes = dataStream.readNBytes(Track.getTrackIdNumChars());
+		char[] trackId = new String(trackIdBytes, StandardCharsets.US_ASCII).toCharArray();
+		byte popularity = dataStream.readByte();
+		byte key = dataStream.readByte();
+		float danceability = dataStream.readFloat();
+		float energy = dataStream.readFloat();
+		float loudness = dataStream.readFloat();
+		float tempo = dataStream.readFloat();
+		float valence = dataStream.readFloat();
+
+		this.track = new Track(albumReleaseDate, genres, trackArtists, albumName, albumType, name,
+				explicit, trackId, loudness, danceability, energy, valence, tempo, key, popularity, id);
+	}
+
+	public ByteArrayInputStream getStream() {
+		return stream;
+	}
+
+	public void setStream(ByteArrayInputStream stream) {
+		this.stream = stream;
+	}
+
+	public Track getTrack() throws IOException {
+		if (track == null)
+			this.readTrack();
+
+		this.stream = null;
+		return track;
+	}
+
+	public void setTrack(Track track) {
+		this.track = track;
+	}
+}
+
 // Classe de escrita para o arquivo.
 class BinaryTrackWriter extends BinaryTrack {
 	protected ByteArrayOutputStream stream;
