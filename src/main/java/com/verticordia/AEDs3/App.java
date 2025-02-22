df2781098c8c364e8de7cbdaa39f0a60bf2941ee
Lucca Pellegrini <632054@sga.pucminas.br>
2025-02-22 18:09:38 -0300

    App: inclui exemplo de uso de operação Read num índice aleatório

--- src/main/java/com/verticordia/AEDs3/App.java
+++ src/main/java/com/verticordia/AEDs3/App.java
@@ -2,6 +2,7 @@ package com.verticordia.AEDs3;
 
 import com.verticordia.AEDs3.util.CSVManager;
 import java.io.IOException;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class App {
 	public static void main(String[] args) throws IOException {
@@ -10,5 +11,7 @@ public class App {
 
 		for (Track track : csv)
 			db.create(track);
+
+		System.out.println(db.read(ThreadLocalRandom.current().nextInt(1, 99891)));
 	}
 }
