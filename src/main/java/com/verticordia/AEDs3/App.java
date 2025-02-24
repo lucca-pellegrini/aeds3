3d7d134d9b7f624863fcaa8793131481d2873b2d
Lucca Pellegrini <632054@sga.pucminas.br>
2025-02-24 14:07:06 -0300

    App: adiciona exemplo de uso do método `delete()`

--- src/main/java/com/verticordia/AEDs3/App.java
+++ src/main/java/com/verticordia/AEDs3/App.java
@@ -4,9 +4,12 @@ import com.verticordia.AEDs3.DataBase.CSVManager;
 import com.verticordia.AEDs3.DataBase.Track;
 import com.verticordia.AEDs3.DataBase.TrackDB;
 import java.io.IOException;
+import java.util.NoSuchElementException;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class App {
+	public static final int ID = ThreadLocalRandom.current().nextInt(1, 99891);
+
 	public static void main(String[] args) throws IOException {
 		CSVManager csv = new CSVManager("dataset-clean.csv");
 		TrackDB db = new TrackDB("tracks.db");
@@ -14,6 +17,14 @@ public class App {
 		for (Track track : csv)
 			db.create(track);
 
-		System.out.println(db.read(ThreadLocalRandom.current().nextInt(1, 99891)));
+		System.out.println(db.read(ID));
+		db.delete(ID);
+
+		try {
+			db.read(ID);
+			throw new RuntimeException("Erro: ID " + ID + " deveria ter sido deletado!");
+		} catch (NoSuchElementException e) {
+			System.out.println("ID " + ID + " deletado com sucesso");
+		}
 	}
 }
