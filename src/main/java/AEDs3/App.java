1d3a96c613f9aa42752f476f532aeae16052bb69
Lucca Pellegrini <632054@sga.pucminas.br>
2025-02-26 11:04:59 -0300

    App: atualiza exemplo para escolher qualquer ID presente no DB

--- src/main/java/AEDs3/App.java
+++ src/main/java/AEDs3/App.java
@@ -4,17 +4,18 @@ import AEDs3.DataBase.CSVManager;
 import AEDs3.DataBase.Track;
 import AEDs3.DataBase.TrackDB;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class App {
-	public static final int ID = ThreadLocalRandom.current().nextInt(1, 99891);
-
 	public static void main(String[] args) throws IOException {
 		CSVManager csv = new CSVManager("dataset-clean.csv");
 		TrackDB db = new TrackDB("tracks.db");
 
 		for (Track track : csv)
 			db.create(track);
+
+		final int ID = ThreadLocalRandom.current().nextInt(1, db.getLastId());
 
 		System.out.println(db.read(ID));
 		db.delete(ID);
