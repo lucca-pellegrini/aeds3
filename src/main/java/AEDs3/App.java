ce2058c2a1e2af212fdcee500efcab15cd51e2bb
Lucca Pellegrini <632054@sga.pucminas.br>
2025-03-08 18:47:52 -0300

    App: adiciona comentários Javadoc

--- src/main/java/AEDs3/App.java
+++ src/main/java/AEDs3/App.java
@@ -1,6 +1,24 @@
 package AEDs3;
 
+/**
+ * Classe principal do programa que inicia a interface de linha de comando.
+ *
+ * A classe {@link App} contém o método {@link #main(String[])} que é o ponto de
+ * entrada
+ * do aplicativo. Ao ser executada, ela cria uma nova instância da classe
+ * {@link CommandLineInterface} com os argumentos fornecidos.
+ */
 public class App {
+	/**
+	 * Método principal do programa.
+	 *
+	 * Este método é o ponto de entrada do aplicativo e é responsável por iniciar
+	 * a interface de linha de comando. Ele cria uma nova instância da classe
+	 * {@link CommandLineInterface}, passando os argumentos fornecidos na execução.
+	 *
+	 * @param args Argumentos de linha de comando fornecidos na execução do
+	 *             programa.
+	 */
 	public static void main(String[] args) {
 		new CommandLineInterface(args);
 	}
