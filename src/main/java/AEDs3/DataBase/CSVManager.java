e0c19336ca6b6c9bb011a5bc7c0eeca12827f513
Lucca Pellegrini <632054@sga.pucminas.br>
2025-05-01 04:01:33 -0300

    CSVManager: remove import não utilizado

--- src/main/java/AEDs3/DataBase/CSVManager.java
+++ src/main/java/AEDs3/DataBase/CSVManager.java
@@ -10,7 +10,6 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.stream.Collectors;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
