c186e3df203a8b9959913875e0cd2d352672a0d0
Lucca Pellegrini <632054@sga.pucminas.br>
2025-06-08 12:26:27 -0300

    CommandLineInterface: mostra banner nova no Windows
    Aparentemente, o comando de mudança de página `chcp 65001` que
    introduzimos em e1d6ca2 basta para exibir os banners novos corretamente.
    Parece que eu não testei direito... :P

--- src/main/java/AEDs3/CommandLineInterface.java
+++ src/main/java/AEDs3/CommandLineInterface.java
@@ -1944,9 +1944,7 @@ public class CommandLineInterface {
 
 			// Informa sobre o fim da execução.
 			commands.info("Programa finalizado");
-			if (App.OS.contains("win")) {
-				commands.hint("Tente executar no Linux ou no macOS para ver as banners novas.");
-			} else if (!this.welcomeBannerShown) {
+			if (!this.welcomeBannerShown) {
 				commands.hint("A janela do seu terminal é muito pequena, então você não viu nossa banner!");
 				commands.hint("Tente executar em uma janela maior na próxima vez, se quiser apreciar ;)");
 			}
@@ -1979,12 +1977,6 @@ public class CommandLineInterface {
 	 * </p>
 	 */
 	public void showWelcomeBanner() throws IOException {
-		// No Windows, os caracteres unicode nos arquivos ANSI não funcionam.
-		if (App.OS.contains("win")) {
-			showWindowsWelcomeBanner();
-			return;
-		}
-
 		// Não exibe o banner se o terminal for muito pequeno.
 		if (terminal.getWidth() < App.MIN_TERMINAL_WIDTH || terminal.getHeight() < App.MIN_TERMINAL_HEIGHT)
 			return;
@@ -2059,54 +2051,6 @@ public class CommandLineInterface {
 		terminal.writer().println(sep.toString());
 
 		this.welcomeBannerShown = true;
-	}
-
-	private void showWindowsWelcomeBanner() {
-		// Cria a arte ASCII à esquerda com cores.
-		String[] bannerLeft = new String[8];
-		bannerLeft[0] = "";
-		bannerLeft[1] = "▄▄▄▄▄▄▄                      █      ▄▄▄▄   ▄▄▄▄▄   ";
-		bannerLeft[2] = "   █     ▄ ▄▄   ▄▄▄    ▄▄▄   █   ▄  █   ▀▄ █    █  ";
-		bannerLeft[3] = "   █     █▀  ▀ ▀   █  █▀  ▀  █ ▄▀   █    █ █▄▄▄▄▀  ";
-		bannerLeft[4] = "   █     █     ▄▀▀▀█  █      █▀█    █    █ █    █  ";
-		bannerLeft[5] = "   █     █     ▀▄▄▀█  ▀█▄▄▀  █  ▀▄  █▄▄▄▀  █▄▄▄▄▀  ";
-		bannerLeft[6] = "";
-		bannerLeft[7] = "══════════════════════════════════════════════════════════════════════\n";
-
-		// Aplica a formatação de cor nas linhas da arte ASCII.
-		for (int i = 1; i <= 5; ++i)
-			bannerLeft[i] = new AttributedString(
-					bannerLeft[i], AttributedStyle.BOLD.foreground(AttributedStyle.GREEN))
-					.toAnsi();
-		bannerLeft[7] = new AttributedString(
-				bannerLeft[7], AttributedStyle.BOLD.foreground(AttributedStyle.MAGENTA))
-				.toAnsi();
-
-		// Cria a descrição do programa à direita.
-		String[] bannerRight = new String[5];
-		bannerRight[0] = "Banco de Dados";
-		bannerRight[1] = "de Faixas Musicais";
-		bannerRight[2] = "em formato binário";
-		bannerRight[3] = "Lucca Pellegrini";
-		bannerRight[4] = "Pedro Vitor Andrade";
-
-		// Aplica a formatação de cor nas linhas de descrição.
-		for (int i = 0; i <= 2; ++i)
-			bannerRight[i] = new AttributedString(
-					bannerRight[i], AttributedStyle.DEFAULT.foreground(AttributedStyle.CYAN))
-					.toAnsi();
-		for (int i = 3; i <= 4; ++i)
-			bannerRight[i] = new AttributedString(
-					bannerRight[i], AttributedStyle.DEFAULT.foreground(AttributedStyle.YELLOW))
-					.toAnsi();
-
-		// Combina a arte ASCII com a descrição do programa.
-		for (int i = 0; i <= 4; ++i)
-			bannerLeft[i + 1] += bannerRight[i];
-
-		// Exibe o banner completo no terminal.
-		for (String s : bannerLeft)
-			terminal.writer().println(s);
 	}
 
 	static class FileCompleter implements Iterable<String> {
