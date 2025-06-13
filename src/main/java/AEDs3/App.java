ea528dea0b862041fe37264599a5d06a77181681
Lucca Pellegrini <632054@sga.pucminas.br>
2025-06-13 19:30:03 -0300

    App: aplica formatação aos Javadocs

--- src/main/java/AEDs3/App.java
+++ src/main/java/AEDs3/App.java
@@ -11,9 +11,8 @@ import java.util.Random;
  * Classe principal do programa que inicia a interface de linha de comando.
  * <p>
  * A classe {@link App} contém o método {@link #main(String[])} que é o ponto de
- * entrada
- * do aplicativo. Ao ser executada, ela cria uma nova instância da classe
- * {@link CommandLineInterface} com os argumentos fornecidos.
+ * entrada do aplicativo. Ao ser executada, ela cria uma nova instância da
+ * classe {@link CommandLineInterface} com os argumentos fornecidos.
  */
 public class App {
 	/**
