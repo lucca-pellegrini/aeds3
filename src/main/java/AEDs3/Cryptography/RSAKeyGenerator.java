53aa69578fca0f460786ba30f748182598526dc1
Lucca Pellegrini <632054@sga.pucminas.br>
2025-06-28 15:59:55 -0300

    RSA: achata pacote para que se contenha no de criptografa em um nível apenas

---
+++ src/main/java/AEDs3/Cryptography/RSAKeyGenerator.java
@@ -0,0 +1,43 @@
+package AEDs3.Cryptography;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Classe responsável por gerar e salvar chaves RSA em arquivos binários.
+ * Esta classe utiliza o algoritmo RSA para criar um par de chaves (pública e
+ * privada) e as armazena nos caminhos de arquivos especificados.
+ */
+public class RSAKeyGenerator {
+
+	/**
+	 * Gera um par de chaves RSA e salva nos arquivos especificados.
+	 *
+	 * @param privateKeyPath Caminho do arquivo onde a chave privada será salva.
+	 * @param publicKeyPath  Caminho do arquivo onde a chave pública será salva.
+	 * @throws IOException      Se ocorrer um erro durante o salvamento das chaves
+	 *                          nos arquivos.
+	 * @throws RuntimeException Se o algoritmo RSA não estiver disponível no
+	 *                          ambiente de execução.
+	 */
+	public static void generateKeys(String privateKeyPath, String publicKeyPath) throws IOException {
+		try {
+			KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+			generator.initialize(2048);
+			KeyPair keyPair = generator.generateKeyPair();
+
+			try (FileOutputStream out = new FileOutputStream(publicKeyPath)) {
+				out.write(keyPair.getPublic().getEncoded());
+			}
+
+			try (FileOutputStream out = new FileOutputStream(privateKeyPath)) {
+				out.write(keyPair.getPrivate().getEncoded());
+			}
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
